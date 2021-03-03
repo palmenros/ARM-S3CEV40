78245a139dd12a589fa79f55d013b36e2de476bb
Pedro Palacios Almendros <palmenros@gmail.com>
2021-03-03 19:16:47 +0100

    Prac1a

---
+++ prac1a/gpio.c
@@ -0,0 +1,155 @@
+#include "44b.h"
+#include "gpio.h"
+
+/* Port B interface implementation */
+
+int portB_conf(int pin, enum port_mode mode)
+{
+	int ret = 0;
+	if (pin < 0 || pin > 10)
+		return -1; // indica error
+
+	if (mode == SIGOUT)
+		// COMPLETAR: poner en rPCONB el bit indicado por pin a 1 para que por
+		// dicho pin en el puerto B salga la seÃ±al correspondiente del
+		// controlador de memoria
+		rPCONB |= 1 << pin;
+	else if (mode == OUTPUT)
+		// COMPLETAR: poner en rPCONB el bit indicado por pin a 0 para que dicho
+		// pin sea un pin de salida
+		rPCONB &= ~(1 << pin);
+	else
+		ret = -1; // indica error
+
+	return ret;
+}
+
+int portB_write(int pin, enum digital val)
+{
+	if (pin < 0 || pin > 10)
+		return -1; // indica error
+
+	if (val < 0 || val > 1)
+		return -1; // indica error
+
+	if (val)
+		// COMPLETAR: poner en rPDATB el bit indicado por pin a 1
+		rPDATB |= 1 << pin;
+	else
+		// COMPLETAR: poner en rPDATB el bit indicado por pin a 0
+		rPDATB &= ~(1 << pin);
+
+	return 0;
+}
+
+/* Port G interface implementation */
+
+int portG_conf(int pin, enum port_mode mode)
+{
+	int pos  = pin*2;
+
+	if (pin < 0 || pin > 7)
+		return -1; // indica error
+
+	switch (mode) {
+		case INPUT:
+			// COMPLETAR: poner en rPCONG 00 a partir de la posiciÃ³n pos para
+			// configurar como pin de entrada el pin indicado por el parÃ¡metro pin
+			rPCONG &= ~(1 << pos);  	 // 0
+			rPCONG &= ~(1 << (pos + 1)); // 0
+			break;
+		case OUTPUT:
+			// COMPLETAR: poner en rPCONG 01 a partir de la posiciÃ³n pos para
+			// configurar como pin de salida el pin indicado por el parÃ¡metro pin
+			rPCONG |= 1 << pos;			 // 1
+			rPCONG &= ~(1 << (pos + 1)); // 0
+			break;
+		case SIGOUT:
+			// COMPLETAR: poner en rPCONG 10 a partir de la posiciÃ³n pos para
+			// que salga la seÃ±al interna correspondiente por el pin indicado
+			// por el parÃ¡metro pin
+			rPCONG &= ~(1 << pos); 		// 0
+			rPCONG |= 1 << (pos + 1);	// 1
+			break;
+		case EINT:
+			// COMPLETAR: poner en rPCONG 11 a partir de la posiciÃ³n pos para
+			// habilitar la generaciÃ³n de interrupciones externas por el pin
+			// indicado por el parÃ¡metro pin
+			rPCONG |= 1 << pos;			// 1
+			rPCONG |= 1 << (pos + 1);	// 1
+			break;
+		default:
+			return -1;
+	}
+
+	return 0;
+}
+
+int portG_eint_trig(int pin, enum trigger trig)
+{
+	// A COMPLETAR EN LA SIGUIENTE DE LA PRÃ�CTICA
+	return 0;
+}
+
+int portG_write(int pin, enum digital val)
+{
+	int pos = pin*2;
+
+	if (pin < 0 || pin > 7)
+		return -1; // indica error
+
+	if (val < 0 || val > 1)
+		return -1; // indica error
+
+	if ((rPCONG & (0x3 << pos)) != (0x1 << pos))
+		return -1; // indica error
+
+	if (val)
+		// COMPLETAR: poner en rPDATG el bit indicado por pin a 1
+		rPDATG |= 1 << pin;
+	else
+		// COMPLETAR: poner en rPDATG el bit indicado por pin a 0
+		rPDATG &= ~(1 << pin);
+
+	return 0;
+}
+
+int portG_read(int pin, enum digital* val)
+{
+	int pos = pin*2;
+
+	if (pin < 0 || pin > 7)
+		return -1; // indica error
+
+	if (rPCONG & (0x3 << pos))
+		return -1; // indica error
+
+	/*COMPLETAR: true si estÃ¡ a 1 en rPDATG el pin indicado por el parÃ¡metro pin*/
+	if (rPDATG & (1 << pin))
+		*val = HIGH;
+	else
+		*val = LOW;
+
+	return 0;
+}
+
+int portG_conf_pup(int pin, enum enable st)
+{
+	if (pin < 0 || pin > 7)
+		return -1; // indica error
+
+	if (st != ENABLE && st != DISABLE)
+		return -1; // indica error
+
+	if (st == ENABLE)
+		// COMPLETAR: poner el pin de rPUPG indicado por el parametro pin al valor adecuado,
+		// para activar la resistencia de pull-up
+		rPUPG &= ~(1 << pin);
+	else
+		// COMPLETAR: poner el pin de rPUPG indicado por el parametro pin al valor adecuado,
+		// para desactivar la resistencia de pull-up
+		rPUPG |= 1 << pin;
+
+	return 0;
+}
+
---
+++ prac1a/main.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include "44b.h"
+#include "button.h"
+#include "leds.h"
+#include "utils.h"
+#include "D8Led.h"
+#include "gpio.h"
+
+struct RLstat {
+	int moving;
+	int speed;
+	int iter;
+	int direction;
+	int position;
+};
+
+static struct RLstat RL = {
+	.moving = 0,
+	.speed = 5,
+	.iter = 0,
+	.direction = 0,
+	.position = 0,
+};
+
+int setup(void)
+{
+	leds_init();
+	D8Led_init();
+	D8Led_segment(RL.position);
+
+	/* Port G: configuración para espera activa */
+
+	//COMPLETAR: utilizando el interfaz para el puerto G definido en gpio.h hay
+	//que configurar los pines 6 y 7 del puerto G como pines de entrada y
+	//activar las correspondientes resistencias de pull-up
+	
+	portG_conf_pup(6, ENABLE);
+	portG_conf(6, INPUT);
+
+	portG_conf_pup(7, ENABLE);
+	portG_conf(7, INPUT);
+
+	/*******************************************/
+
+	Delay(0);
+	return 0;
+}
+
+int loop(void)
+{
+	unsigned int buttons = read_button();
+	unsigned int contador_pulsaciones = 0;
+
+	if (buttons & BUT1) {
+		// COMPLETAR: utilizando la interfaz para los leds definida en leds.h
+		// hay que apagar ambos leds
+		led1_off();
+		led2_off();
+
+		// También hay que conmutar la dirección del movimiento del led rotante
+		// representado por el campo direction de la variable RL
+		RL.direction = !RL.direction;
+	}
+
+	if (buttons & BUT2) {
+		// COMPLETAR: utilizando la interfaz para los leds definida en leds.h
+		// Incrementar contador de pulsaciones. Si es par, conmUtar led1. Si es impar, conmutar el led2.
+
+		contador_pulsaciones++;
+
+		if(contador_pulsaciones & 1)
+			//impar
+			led2_switch();
+		else
+			//par
+			led1_switch();
+
+		// También hay que conmutar el estado de movimiento del led rotante
+		// representado por el campo moving de la variable RL, y en caso de
+		// ponerlo en marcha debemos reiniciar el campo iter al valor del campo
+		// speed.
+
+		RL.moving = !RL.moving;
+		if(RL.moving)
+			RL.iter = RL.speed;
+	}
+
+	if (RL.moving) {
+		RL.iter--;
+		if (RL.iter == 0) {
+			// COMPLETAR: debemos hacer avanzar el led rotante una posición en
+			// la dirección indicada por el campo direction de la variable RL.
+
+
+			// La posición actual está representada en el campo position.
+			// Recordar que queremos un movimiento circular, representado por
+			// las 6 primeras posiciones en el array Segmentes del display de 8
+			// segmentos, por lo que position debe estar siempre entre 0 y 5.
+			
+			//	   0
+			//    __
+			// 5 |  | 1
+			//   |__|
+			//   |  | 2
+			// 4 |__|
+			//     3
+
+			if(RL.direction) {
+				//Horario
+				RL.position++;
+
+				if(RL.position == 6)
+					RL.position = 0;
+
+			} else {
+				//Antihorario
+				RL.position--;
+
+				if(RL.position == -1)
+					RL.position = 5;
+			}
+
+			D8Led_segment(RL.position);
+			RL.iter = RL.speed;
+		}
+	}
+
+	Delay(2000); // espera de 200ms para que el bucle se repita 5 veces por segundo
+	return 0;
+}
+
+
+int main(void)
+{
+	setup();
+
+	while (1) {
+		loop();
+	}
+}
